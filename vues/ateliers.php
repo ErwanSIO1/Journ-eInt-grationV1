518c398309213029e706ac2c35cabb827e465e8d
victor.marie <victor.marie@PC-00710-SJE.stjohnperse.lyc>
2024-09-05 14:57:18 +0200

    Version ticket 2 OK+

--- vues/ateliers.php
+++ vues/ateliers.php
@@ -4,28 +4,15 @@
 <div id="description">
 <h2> Les ateliers envisagés pour la journée d'intégration sont : </h2>
 
-
     <table>
         <tr> <th>Atelier</th> <th>Durée</th> <th>Besoin ordi</th> <th>Capacité</th> </tr>
-        <tr><td><?php
+        <?php
 		foreach ($lesAteliers as $atelier) {
-			echo $atelier->libelle."<br />";
+			echo "<tr> <td>".$atelier->libelle."</td>";
+                        echo "<td>".$atelier->duree."</td>";
+                        echo "<td>".$atelier->besoinordi."</td>";
+                        echo "<td>".$atelier->capacite."</td> </tr>";
 		}
-                ?></td>
-            <td><?php
-		foreach ($lesAteliers as $atelier) {
-			echo $atelier->duree."<br />";
-		}
-                ?></td>
-            <td><?php
-		foreach ($lesAteliers as $atelier) {
-			echo $atelier->besoinordi."<br />";
-		}
-                ?></td>
-            <td><?php
-		foreach ($lesAteliers as $atelier) {
-			echo $atelier->capacite."<br />";
-		}
-                ?></td> </tr>
+                ?>
     </table>
 </div>
